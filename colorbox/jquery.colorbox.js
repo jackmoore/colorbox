c2c0dea5ef7ecd7d84fefee33c8610135ccb19b9
James Bebbington <james.bebbington@gmail.com>
2010-09-20 16:26:11 +0100

    Fix issue with open option not processing function value.

--- colorbox/jquery.colorbox.js
+++ colorbox/jquery.colorbox.js
@@ -227,7 +227,11 @@
 		$this.each(function () {
 			$(this).data(colorbox, $.extend({}, $(this).data(colorbox) || defaults, options)).addClass(boxElement);
 		});
-		
+
+		if ($.isFunction(options.open)) {
+			options.open = options.open.call($this);
+		};
+
 		if (options.open) {
 			launch($this[0]);
 		}
