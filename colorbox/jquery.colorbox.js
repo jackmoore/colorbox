8828e2503c7b3ccb1d6252a1460190719319f291
Laurent Goderre <laurent.goderre@gmail.com>
2013-01-02 14:26:27 -0500

    Removed instance of the deprecated .error() method

--- colorbox/jquery.colorbox.js
+++ colorbox/jquery.colorbox.js
@@ -821,7 +821,7 @@
 		} else if (isImage(href)) {
 			$(photo = new Image())
 			.addClass(prefix + 'Photo')
-			.error(function () {
+			.on('error',function () {
 				settings.title = false;
 				prep($tag(div, 'Error').html(settings.imgError));
 			})
