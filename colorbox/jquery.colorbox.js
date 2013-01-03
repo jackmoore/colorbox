7063473912dd75f136a1e7f93167f36954e99e47
Laurent Goderre <laurent.goderre@gmail.com>
2013-01-03 09:25:29 -0500

    Switched the .on() to .bind() to support both newer and older versions of jQuery

--- colorbox/jquery.colorbox.js
+++ colorbox/jquery.colorbox.js
@@ -821,7 +821,7 @@
 		} else if (isImage(href)) {
 			$(photo = new Image())
 			.addClass(prefix + 'Photo')
-			.on('error',function () {
+			.bind('error',function () {
 				settings.title = false;
 				prep($tag(div, 'Error').html(settings.imgError));
 			})
