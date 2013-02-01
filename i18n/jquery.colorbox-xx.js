4676100d8e34ca51f5f5b9c90ec499b8bcfa7074
Filip Novák <filip.f.novak@gmail.com>
2013-02-01 23:33:00 +0100

    'no language' language file ;-)

---
+++ i18n/jquery.colorbox-xx.js
@@ -0,0 +1,14 @@
+/*
+	jQuery ColorBox language configuration
+	language: "no language"
+	translated by: Filip Novak
+	site: mame.napilno.cz/filip-novak
+*/
+jQuery.extend(jQuery.colorbox.settings, {
+	current: "{current} ({total})",
+	previous: "<<<",
+	next: ">>>",
+	close: "[x]",
+	xhrError: "!!!",
+	imgError: "!!!"
+});
