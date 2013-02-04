362391b33c057e2fc0c984af5beb46e6bdc73514
Reinaldo Martins Thoma <reinaldo.martins@gmail.com>
2013-02-04 13:02:04 -0200

    Added Brazilian Portuguese language configuration

---
+++ i18n/jquery.colorbox-pt-br.js
@@ -0,0 +1,13 @@
+/*
+	jQuery ColorBox language configuration
+	language: Brazilian Portuguese (pt-br)
+	translated by: ReinaldoMT
+*/
+jQuery.extend(jQuery.colorbox.settings, {
+	current: "Imagem {current} de {total}",
+	previous: "Anterior",
+	next: "Próxima",
+	close: "Fechar",
+	xhrError: "Erro ao carregar o conteúdo.",
+	imgError: "Erro ao carregar a imagem."
+});
