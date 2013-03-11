6b63ef2b87965d10ebe0656acc5cf2e15841c707
Jack Moore <jack@colorpowered.com>
2013-03-11 13:51:58 -0400

    Turkish translation

---
+++ i18n/jquery.colorbox-tr.js
@@ -0,0 +1,16 @@
+/*
+	jQuery ColorBox language configuration
+	language: Turkish (tr)
+	translated by: Caner ÖNCEL
+	site: egonomik.com
+*/
+jQuery.extend(jQuery.colorbox.settings, {
+  current: "Resim {current} / {total}",
+  previous: "Önceki",
+  next: "Sonraki",
+  close: "Kapat",
+  xhrError: "İçerik yüklenirken bir hata meydana geldi.",
+  imgError: "Resim yüklenirken bir hata meydana geldi.",
+  slideshowStart: "Slaydı Başlat",
+  slideshowStop: "Slaydı Durdur"
+});
