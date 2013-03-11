b9034e5c09f4d681b427deb17fbfc32ef72fe546
Jack Moore <jack@colorpowered.com>
2013-03-11 12:52:06 -0400

    Persian (Farsi) translation

---
+++ i18n/jquery.colorbox-fa.js
@@ -0,0 +1,18 @@
+/*
+	jQuery ColorBox language configuration
+	language: Persian (Farsi)
+	translated by: Mahdi Jaberzadeh Ansari (MJZSoft)
+	site: www.mjzsoft.ir
+	email: mahdijaberzadehansari (at) yahoo.co.uk
+	Please note : Persian language is right to left like arabic.
+*/
+jQuery.extend(jQuery.colorbox.settings, {
+	current: "فعلی",
+	previous: "قبلی",
+	next: "بعدی",
+	close: "بستن",
+	xhrError: "قابلیت بارگذاری این نوع از داده وجود ندارد.",
+	imgError: "اشکالی در بارگذاری تصویر رخ داده است.",
+	slideshowStart: "آغاز نمایش خودکار",
+	slideshowStop: "توقف نمایش خودکار"
+});
