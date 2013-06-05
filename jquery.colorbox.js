c7a0548a136f6c31ddb823969c65ceb8f7a0511c
Jack Moore <jack@colorpowered.com>
2013-06-05 18:56:54 -0400

    Doh!  Fixing bug/typo from last commit.

--- jquery.colorbox.js
+++ jquery.colorbox.js
@@ -1,5 +1,5 @@
 /*!
-	jQuery Colorbox v1.4.19 - 2013-06-03
+	jQuery Colorbox v1.4.20 - 2013-06-05
 	(c) 2013 Jack Moore - jacklmoore.com/colorbox
 	license: http://www.opensource.org/licenses/mit-license.php
 */
@@ -449,7 +449,7 @@
 		function clickHandler(e) {
 			// ignore non-left-mouse-clicks and clicks modified with ctrl / command, shift, or alt.
 			// See: http://jacklmoore.com/notes/click-events/
-			if (!(e.which > 1 || e.shiftKey || e.altKey || e.metaKey || e.ctrl)) {
+			if (!(e.which > 1 || e.shiftKey || e.altKey || e.metaKey || e.ctrlKey)) {
 				e.preventDefault();
 				launch(this);
 			}
