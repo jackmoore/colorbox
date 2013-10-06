79576cbf6a639d96ac5ec754ac6f847e29a7a5ac
alexandr Belov <roosso@ya.ru>
2013-10-05 21:08:02 -0700

    Update jquery.colorbox-ru.js
    fixed translate

--- i18n/jquery.colorbox-ru.js
+++ i18n/jquery.colorbox-ru.js
@@ -1,16 +1,16 @@
 /*
 	jQuery Colorbox language configuration
 	language: Russian (ru)
-	translated by: Marfa
-	site: themarfa.name
+	translated by: alex Roosso
+	site: www.roocms.com
 */
 jQuery.extend(jQuery.colorbox.settings, {
-	current: "изображение {current} из {total}",
-	previous: "назад",
-	next: "вперёд",
-	close: "закрыть",
-	xhrError: "Не удалось загрузить содержимое.",
-	imgError: "Не удалось загрузить изображение.",
-	slideshowStart: "начать слайд-шоу",
-	slideshowStop: "остановить слайд-шоу"
+	current: "{current} из {total}",
+	previous: "Назад",
+	next: "Далее",
+	close: "Закрыть",
+	xhrError: "Содержимое не загружено!",
+	imgError: "Изобрадение не загрузилось.",
+	slideshowStart: "Воспроизвести",
+	slideshowStop: "Остановить"
 });
