f25a1f729b9f91dba3e7593dede4aded34bd0898
Jack Moore <hello@jacklmoore.com>
2014-02-01 12:26:59 -0500

    updated bower ignore

--- jquery.colorbox.js
+++ jquery.colorbox.js
@@ -1,5 +1,5 @@
 /*!
-	Colorbox v1.4.35 - 2014-01-30
+	Colorbox v1.4.36 - 2014-02-01
 	jQuery lightbox and modal window plugin
 	(c) 2014 Jack Moore - http://www.jacklmoore.com/colorbox
 	license: http://www.opensource.org/licenses/mit-license.php
