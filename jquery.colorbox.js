1e9c55a83c980b1dc0ce90f7e830ae0dba9dfd2f
Дубовик Максим <lufton@gmail.com>
2014-08-13 11:54:03 +0300

    Simplifying code

--- jquery.colorbox.js
+++ jquery.colorbox.js
@@ -733,7 +733,7 @@
 				scrolltop = $loaded.scrollTop();
 				$loaded.css({height: "auto"});
 				settings.h = $loaded.height();
-				if (settings.h > settings.mh) settings.h=settings.mh;
+				settings.h = Math.min(settings.h, settings.mh);
 			}
 
 			$loaded.css({height: settings.h});
