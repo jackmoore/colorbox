1710e79d99a07a7396d01f298d1aa5737ab98d33
Tri Ân <ngtrian@users.noreply.github.com>
2017-03-13 01:13:28 +0700

    Vietnamese translation

---
+++ i18n/jquery.colorbox-vi.js
@@ -0,0 +1,16 @@
+/*
+	jQuery Colorbox language configuration
+	language: Vietnamese (vi)
+	translated by: ngtrian
+	site: nopviet.com
+*/
+jQuery.extend(jQuery.colorbox.settings, {
+	current: "ảnh {current} trên {total}",
+	previous: "trước đó",
+	next: "tiếp theo",
+	close: "đóng",
+	xhrError: "Thất bại tải về nội dung này.",
+	imgError: "Thất bại tải về ảnh này.",
+	slideshowStart: "Bắt đầu trình chiếu",
+	slideshowStop: "Kết thúc trình chiếu"
+});
