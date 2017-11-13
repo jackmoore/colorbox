446e61c1ada040951c21db67ed112d32c6111089
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2017-11-13 22:41:51 +0300

    Create jquery.colorbox-tk.js

---
+++ i18n/jquery.colorbox-tk.js
@@ -0,0 +1,16 @@
+/*
+	jQuery Colorbox language configuration
+	language: Turkmen (tk)
+	translated by: N'Bayramberdiyev
+	site: nbayramberdiyev.com
+*/
+jQuery.extend(jQuery.colorbox.settings, {
+  current: "Surat {current} / {total}",
+  previous: "Öňki",
+  next: "Soňky",
+  close: "Ýap",
+  xhrError: "Ýüklenýärkä näsazlyk ýüze çykdy.",
+  imgError: "Ýüklenýärkä näsazlyk ýüze çykdy.",
+  slideshowStart: "Slaýdy Başlat",
+  slideshowStop: "Slaýdy Duruz"
+});
