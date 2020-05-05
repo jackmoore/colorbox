22eb4c1dc9f78523cee35fa6e6d237f79c1179aa
markus-drupal <48351461+markus-drupal@users.noreply.github.com>
2020-05-06 00:07:40 +0200

    Add redraw() public method.
    This is necesssary to be able to redraw the colorbox on the current slide to force re-position or sixe based on events that are not controlled by dom events. It is not possible to implement such a method from outside the colorbox plugin because it uses internal variables that cannot be accessed from the outter space...

--- jquery.colorbox.js
+++ jquery.colorbox.js
@@ -1051,6 +1051,12 @@
 		}
 	};
 
+	publicMethod.redraw = function () {
+		if (!active && $related[index]) {
+			launch($related[index]);
+		}
+	};
+
 	// Note: to use this within an iframe use the following format: parent.jQuery.colorbox.close();
 	publicMethod.close = function () {
 		if (open && !closing) {
